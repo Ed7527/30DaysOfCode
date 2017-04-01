96f7edd006624b8c0e83dc6f2df310346735afed
Edward Torpy <edward.torpy@my.jcu.edu.au>
2017-04-01 17:27:34 +1000

    Update Day00_Hello_World.java

--- src/pkg30daysofcode/Day00_Hello_World.java
+++ src/pkg30daysofcode/Day00_Hello_World.java
@@ -8,7 +8,7 @@ public class Day00_Hello_World {
         
         // Create a Scanner object to read input from stdin.
 	Scanner scan = new Scanner(System.in); 
-		
+	//	
 	// Read a full line of input from stdin and save it to our variable, inputString.
 	String inputString = scan.nextLine(); 
 
